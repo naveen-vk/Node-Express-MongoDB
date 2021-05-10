a1c6a18093d3a6496dbea2f2455db39254cd6ecc
Naveen17euit093 <17euit093@skcet.ac.in>
2021-05-10 19:12:19 +0530

    Initial Commit

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+console.log(textIn);
+const textOut = `This is what we know about the avacado:.\n${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log("File Written");
